dcc78e61aa96cf30cc18735c2058b697bc878816
EvioneSheyn <155628267+EvioneSheyn@users.noreply.github.com>
2025-05-30 11:17:23 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgRBXCl2pAGyXWVT2g43UXiT1cIwcQy-g",
+  authDomain: "gymtechsystem-333e9.firebaseapp.com",
+  projectId: "gymtechsystem-333e9",
+  storageBucket: "gymtechsystem-333e9.firebasestorage.app",
+  messagingSenderId: "966604695234",
+  appId: "1:966604695234:web:c5dafd1aee1f0b5522fd5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
